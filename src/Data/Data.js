39207e3a0c5573fafe0c4a089641354b761ca0a0
MHTech229 <mehintocharbel@gmail.com>
2023-12-16 13:46:56 +0100

    0.01

---
+++ src/Data/Data.js
@@ -0,0 +1,32 @@
+const MenuImgs = [
+    "/Img/1.png",
+    "/Img/2.png",
+    "/Img/3.png",
+    "/Img/4.png",
+    "/Img/5.png",
+    "/Img/6.png",
+    "/Img/7.png",
+    "/Img/8.png",
+    "/Img/9.png",
+    "/Img/10.png",
+    "/Img/11.png",
+    "/Img/12.png",
+    "/Img/13.png",
+    "/Img/14.png",
+    "/Img/15.png",
+    "/Img/16.png",
+    "/Img/17.png",
+    "/Img/18.png",
+    "/Img/19.png",
+    "/Img/20.png",
+    "/Img/21.png",
+    "/Img/22.png",
+    "/Img/23.png",
+    "/Img/24.png",
+    "/Img/25.png",
+    "/Img/26.png",
+];
+
+export {
+    MenuImgs
+}
